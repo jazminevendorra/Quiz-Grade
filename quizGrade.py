83edc62f8250fae60e488370744ffe9fcf7520ba
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-02 12:18:55 -0400

    Update quizGrade.py

--- quizGrade.py
+++ quizGrade.py
@@ -1,8 +1,6 @@
 # This program will automatically grade a quiz.
 # This quiz has 15 multiple choice questions.
-# The number of questions may be easily modified, as it is a global variable/
-#
-# Jazmin Even Dorra
+# The number of questions may be easily modified, as it is a global variable
 
 NUM_QUESTIONS = 15
 
